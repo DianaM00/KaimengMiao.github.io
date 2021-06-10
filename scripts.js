789083f383a3e6f2e396458de66a7574ad0d5d48
DianaM00 <79963201+DianaM00@users.noreply.github.com>
2021-06-11 03:43:04 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/* Homepage */
+function myFunction() {
+    document.getElementById("product3-go").innerHTML="Productpage.html";
+}
+
+/* Product Page*/
+function myFunction() {
+    document.getElementById("product-btn1").innerHTML="ShoppingCartPage.html";
+}
+function myFunction() {
+    document.getElementById("product-btn2").innerHTML="CheckoutPage.html";
+}
+
+/* ShoppingCart page*/
+function myFunction() {
+    document.getElementById("purchase-1").innerHTML="CheckoutPage.html";
+}
+function myFunction() {
+    document.getElementById("purchase-2").innerHTML="CheckoutPage.html";
+}
